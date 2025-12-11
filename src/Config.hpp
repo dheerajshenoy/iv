273ea31affe9b4e31aceb6a23070b6b9813b3ccb
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2025-12-11 16:41:26 +0100

    Add minimap UI options and rename field

--- src/Config.hpp
+++ src/Config.hpp
@@ -9,16 +9,25 @@ struct Config
     struct UI
     {
         bool minimap_shown{true};
-        bool auto_hide_minimap{true};
+        bool minimap_auto_hide{true};
+        bool minimap_image{true};
+        float minimap_scale{0.2f};
+        float minimap_image_opacity{0.7f};
+        QString minimap_position{"bottom-right"};
+
         QString minimap_overlay_color{"#55FF0000"};
         QString minimap_overlay_border_color{"#5500FF00"};
         int minimap_overlay_border_width{1};
+
         bool vscrollbar_shown{true};
         bool vscrollbar_auto_hide{true};
+
         bool hscrollbar_shown{true};
         bool hscrollbar_auto_hide{true};
+
         bool tabs_shown{true};
         bool tabs_autohide{true};
+
         bool statusbar_shown{true};
         bool menubar_shown{true};
     };
